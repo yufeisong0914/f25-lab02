114abe4403a67193e5880059822055ccb1959d43
yufei <yufeison@andrew.cmu.edu>
2025-09-03 13:51:56 -0400

    updated main
    Renderer can take any objects that implements Shape interface; Main can declare Shape

--- java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
+++ java/src/main/java/edu/cmu/cs/cs214/lab02/Main.java
@@ -1,10 +1,13 @@
 package edu.cmu.cs.cs214.lab02;
 
 import edu.cmu.cs.cs214.lab02.shapes.Rectangle;
+// import Shape
+import edu.cmu.cs.cs214.lab02.shapes.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        Rectangle rectangle = new Rectangle(2, 3);
+        // declare as Shape
+        Shape rectangle = new Rectangle(2, 3);
 
         Renderer renderer = new Renderer(rectangle);
 
