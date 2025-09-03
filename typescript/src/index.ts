47ab7ad041b1d2157274134949a96e191f42e08d
yufei <yufeison@andrew.cmu.edu>
2025-09-03 14:37:15 -0400

    updated index, declare as Shape

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,6 +1,7 @@
-import { newRenderer } from "./renderer.js"
-import { Rectangle, newRectangle } from "./shapes/rectangle.js";
+import { newRenderer } from "./renderer"
+import { Shape } from "./shapes/shape"
+import { Rectangle } from "./shapes/rectangle"
 
-const rectangle: Rectangle = newRectangle(2, 3)
+const rectangle: Shape = new Rectangle(2, 3)
 const renderer = newRenderer(rectangle)
 renderer.draw();
